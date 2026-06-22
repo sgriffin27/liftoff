43412eb10b4c65354bd910e65ea8923fde3afa9a
sgriffin27 <seg75580@uga.edu>
2026-06-22 16:03:20 -0400

    Create liftoff.sh

---
+++ liftoff.sh
@@ -0,0 +1,3 @@
+module spider Liftoff/1.6.3
+liftofftools all -r /home/seg75580/AfuGenomes/AF293_AspergillusFumagatis_GCF_000002655.1GCF_000002655.1_ASM265v1_genomic.fna -t /home/seg75580/AfuGenomes/GCF_000002655.1_ASM265v1_TR46.fasta /
+-rg /home/seg75580/AfuGenomes/AF293_AspergillusFumagatis_GCF_000002655.1/GCF_000002655.1_ASM265v1_genomic.gff -tg /home/seg75580/AfuGenomes/GCF_000002655.1_ASM265v1_TR46.gff
