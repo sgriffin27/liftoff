f3227f1c9d0511f9dd93f6586ea1c510d7112ca2
sgriffin27 <seg75580@uga.edu>
2026-06-23 14:18:36 -0400

    Update liftoff.sh

--- liftoff.sh
+++ liftoff.sh
@@ -14,6 +14,10 @@ cd $SLURM_SUBMIT_DIR
 
 module load Liftoff/1.6.3
 module load minimap2/2.24-GCCcore-11.3.0
-liftoff -g /home/seg75580/AfuGenomes/AF293_AspergillusFumagatis_GCF_000002655.1/GCF_000002655.1_ASM265v1_genomic.gff -f /scratch/seg75580/liftoff/features.txt /home/seg75580/AfuGenomes/GCF_000002655.1_ASM265v1_TR46.gff /
-/home/seg75580/AfuGenomes/AF293_AspergillusFumagatis_GCF_000002655.1/GCF_000002655.1_ASM265v1_genomic.fna -o /scratch/seg75580/liftoff/TR46_liftover.gff
+
+liftoff -g /home/seg75580/AfuGenomes/genomicver2.gff -f /scratch/seg75580/liftoff/features.txt /home/seg75580/AfuGenomes/GCF_000002655.1_ASM265v1_TR46.fasta /home/seg75580/AfuGenomes/GCF_000002655.1_ASM265v1_genomicver2.fasta -o /scratch/seg75580/liftoff/TR46_liftover.gff
+
+
+
+
 
